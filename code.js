d0a2d0c558cb0552f602d302a3da569065ec8557
SaschaSimic345 <133647048+SaschaSimic345@users.noreply.github.com>
2024-06-04 13:36:30 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+function umrechnen (){
+    var celsius = document.berechnen.celsius.value;
+    var fahrenheit = celsius * 1.8 + 32;
+    alert (fahrenheit);
+}
+function  ausgeben (){
+    alert ("nonsense")
+}
